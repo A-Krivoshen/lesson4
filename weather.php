fffd925f50168f7ef5f4455892d72f7e14fc5801
A-Krivoshein <a.krivoshein@gmail.com@>
2017-09-24 00:18:22 +0300

    update

--- weather.php
+++ weather.php
@@ -3,13 +3,14 @@ $cache_lifetime = 3600;
 $filename = 'cache.txt';
 $json = file_get_contents('http://api.openweathermap.org/data/2.5/weather?id=524901&appid=9c24c2f989b4147c64c0ebf8d7a3f614');
 if (file_exists($filename)) {
- if ( time()-filemtime($filename) < $cache_lifetime )  {
+ if (( time()-filemtime($filename)) >= $cache_lifetime )  {
   $allData = json_decode($json, true);
 }
 
 	else {
 		$cacheWeather = file_get_contents($filename);
-		$allData = json_decode($json, true);
+		echo 'exist';
+                 $allData = json_decode($cacheWeather, true);
 	}
 }
  else {
@@ -18,7 +19,6 @@ if (file_exists($filename)) {
      file_put_contents($filename, $string_data); 	  
  	  }
 
-$allData = json_decode($json, true);
 echo 'name:' . $allData['name'] . "<br>" .
     'weather: ' . $allData['weather'][0]['main'] . "<br>" .
     'description: ' . $allData['weather'][0]['description'] . "<br>" .
