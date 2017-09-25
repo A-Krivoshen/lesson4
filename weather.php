13f8b5d384083df7a2f1128ec8e632872ce996f3
A-Krivoshein <a.krivoshein@gmail.com@>
2017-09-25 23:11:57 +0300

    renew

--- weather.php
+++ weather.php
@@ -1,20 +1,20 @@
 <?php
 $cache_lifetime = 3600;
 $filename = 'cache.txt';
-$json = file_get_contents('http://api.openweathermap.org/data/2.5/weather?id=524901&appid=9c24c2f989b4147c64c0ebf8d7a3f614');
 if (file_exists($filename)) {
- if (( time()-filemtime($filename)) >= $cache_lifetime )  {
-  $allData = json_decode($json, true);
-}
+        if (( time()-filemtime($filename)) >= $cache_lifetime )  {
+              $json = file_get_contents('http://api.openweathermap.org/data/2.5/weather?id=524901&appid=9c24c2f989b4147c64c0ebf8d7a3f614');
+              $allData = json_decode($json, true);
+        }
 
 	else {
 		$cacheWeather = file_get_contents($filename);
-		echo 'exist';
-                 $allData = json_decode($cacheWeather, true);
+      $allData = json_decode($cacheWeather, true);
 	}
 }
  else {
-      $allData = json_decode($json, true); 	  
+     $json = file_get_contents('http://api.openweathermap.org/data/2.5/weather?id=524901&appid=9c24c2f989b4147c64c0ebf8d7a3f614');     
+     $allData = json_decode($json, true); 	  
      $string_data = serialize($allData);
      file_put_contents($filename, $string_data); 	  
  	  }
